4a5a9af4561c9c9c6f8bfa37e3036f1040d896f0
mgalan22 <matiasgalan1@gmail.com>
2022-09-02 20:06:09 -0300

    nada

--- blog/urls.py
+++ blog/urls.py
@@ -13,20 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import view_all_posts, home, add_post
+from webapp.views import about_me, del_post, edit_post, new_user, read_more, view_all_posts, home, add_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/',home, name='home'),
-    #path('about/', about_me),
-    #path('pages/<pageId>',leer_mas),
-    #path('alta/', alta_usuario),
+    path('about/', about_me, name='about'),
+    path('pages/<pageId>',read_more, name='read_more'),
+    path('alta/', new_user, name='new_user'),
     path('post/<str:title>/<str:content>', add_post,name='add_post'),
-    #path('edit_post/', editar_publicacion),
-    #path('del_post/', del_publicacion),
+    path('edit_post/', edit_post, name='edit_post'),
+    path('del_post/', del_post, name='del_post'),
     path('blog/', view_all_posts,name='view_all_posts'),
 
 ]
